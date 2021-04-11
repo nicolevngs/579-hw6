76ca40784e03f60caa71eac98b25467b0436177b
Nicole <nicolevaldespinonogues@MacBook-Pro-de-Nicole.local>
2021-04-11 16:39:45 -0400

    Yay! Final implementations and debugging

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@
 // PROBLEM SET5: LIBRARY ONE USED: libphonenumber (https://gitlab.com/catamphetamine/libphonenumber-js/#cdn) ---- Used as a user input to re-format phone numbers :)
 // SECOND LIBRARY USED: swiper (https://swiperjs.com/) for image carrousel
 
-import {AsYouType} from 'libphonenumber-js';
-import Swiper from 'swiper';
-
 var block = document.getElementById('band1');
+
+var CorrectAnswer;
+var selectedAnswer;
+var points= 0;
+var rounds = 0;
 
 block.addEventListener ("click" , changeColor)
 
@@ -27,6 +29,138 @@ function changeColor(){
 }
 
 
+// Function to get a random item from an array
+function getRandomItem(arr) {
+
+    // get random index value
+    const randomIndex = Math.floor(Math.random() * arr.length);
+
+    // get random item
+    const item = arr[randomIndex];
+
+    return item;
+}
+
+function onSelect(element) {
+    selectedAnswer = element.value;
+}
+
+function onSubmit() {
+    rounds += 1;
+    if(rounds === 6) {
+        if (selectedAnswer === CorrectAnswer){
+         points +=1;   
+        }
+        document.getElementById("totalPoints").innerHTML = "Final Score: "+ points + " points";
+        document.getElementById("exampleModalLabel").innerHTML = "GAME OVER!";
+        document.getElementById("playAgain").style.display = "block";
+        $('#exampleModal').modal('toggle');
+        rounds = 0
+        points = 0
+        return 
+
+    }
+
+    if (selectedAnswer == CorrectAnswer) {
+        points += 1;
+        document.getElementById("totalPoints").innerHTML = "Score: "+ points + " points";
+        document.getElementById("exampleModalLabel").innerHTML = "PAW-RFECT! That is correct!";
+        $('#exampleModal').modal('toggle')
+    }
+    else{
+        document.getElementById("totalPoints").innerHTML = `<div>Correct answer: ${CorrectAnswer}</div> <div>Score: ${points} points</div>`;
+        document.getElementById("exampleModalLabel").innerHTML = `W-R-O-N-G! `;
+
+        $('#exampleModal').modal('toggle')
+
+    }
+}
+
+
+function getBreeds() {
+    fetch('https://api.thedogapi.com/v1/breeds')
+        .then(response => {
+            if (!response.ok) {
+                throw Error("Woof! There's something wrong");
+            }
+            return response.json();
+        })
+        .then (data => {
+            console.log(data);
+            let OneElement = getRandomItem(data)
+            const html =  
+                `
+                <div class="breed">
+                <p style="padding-top: 42px;" ><img src="${OneElement.image.url}" width="400"; /></p>
+                <p style="color: white;" > HINT! <p>
+                <p style="color: white;" > Breed group: ${OneElement.breed_group} </p>
+                </div>
+                `;
+            CorrectAnswer = OneElement.name;
+
+            let IncorrectAnswerOne= getRandomItem(data).name;
+            let IncorrectAnswerTwo= getRandomItem(data).name;
+
+            let choices = [
+                {Label: "Choice1", Value: "one"} ,
+                {Label: "Choice2", Value: "two"},
+                {Label: "Choice3", Value: "three"}
+            ];
+
+            function shuffleArray(array) {
+                for (var i = array.length - 1; i > 0; i--) {
+                    var j = Math.floor(Math.random() * (i + 1));
+                    var temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+
+            shuffleArray(choices)
+
+            let RandomChoice = choices.pop() //grabbing the last element from "choices" and taking it out of the array
+
+            document.getElementById(RandomChoice.Label).innerHTML = CorrectAnswer; 
+            document.getElementById(RandomChoice.Value).value = CorrectAnswer;
+
+            // Choices has now two elements
+
+            RandomChoice = choices.pop() 
+
+            document.getElementById(RandomChoice.Label).innerHTML = IncorrectAnswerOne; 
+            document.getElementById(RandomChoice.Value).value = IncorrectAnswerOne;
+
+            // Choices has now one element :(
+
+            RandomChoice = choices.pop() 
+
+            document.getElementById(RandomChoice.Label).innerHTML = IncorrectAnswerTwo; 
+            document.getElementById(RandomChoice.Value).value = IncorrectAnswerTwo;
+
+
+           
+            
+            
+
+            document.getElementById('dogImage').innerHTML = html;
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+ 
+getBreeds();
+
+// when modal is closed, reload new image (breed). Call function getBreeds again
+$('#exampleModal').on('hidden.bs.modal', function (e) {
+    getBreeds();
+    document.getElementById("playAgain").style.display = "none";
+    /// uncheck all radio buttons when closing modal
+    document.getElementById("one").checked = false;
+    document.getElementById("two").checked = false;
+    document.getElementById("three").checked = false;
+})
+
 var SubmitButton = document.getElementById("guestName")
 var theName = document.getElementById("Userinput")
 
@@ -34,46 +168,9 @@ SubmitButton.addEventListener('click',  GreetMessage )
 
 function GreetMessage(){
     
-    alert("Hello " + theName.value + " nice to meet you!");
+    alert(" Hi there " + theName.value + ", we'll display random images of dogs below. Choose the right breed to gain points. Each game has 6 rounds, but don't worry you can play as many times as you want. Have fun! ");
 
 }
 
-var block2= document.getElementById("band2")
-block2.addEventListener("mousemove", ChangeBorder);
-
-function ChangeBorder() {
-    block2.style.border = "solid 10px black";
-  }
 
 
-var HideButton = document.getElementById("HideButton")
-var theTable = document.getElementById("table")
-
-HideButton.addEventListener ("click" , HideTable)
-
-function HideTable() {
-    theTable.parentNode.removeChild(theTable)
-}
-
-var phone = document.getElementById("number-input");
-phone.addEventListener ("input" , EditNumber)
-console.log(phone);
-
-function EditNumber(e) {
-    console.log(this.value);
-    let formatted = new AsYouType('US').input(this.value);
-    this.value = formatted;
-}
-
-const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-  });
